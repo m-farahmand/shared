ef522ca09bac14bd87a18f8e6cf9f4dddfdca3cb
CESAR CAÑAS CASELLA <cesarbmx@hotmail.com>
2020-05-14 07:48:52 -0400

    ToAlias

---
+++ CesarBmx.Shared.Common/Extensions/TypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CesarBmx.Shared.Common.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static string ToAlias(this Type type)
+        {
+            if (type.ToString() == "System.String") return "string";
+            if (type.ToString() == "System.Int32") return "int";
+            if (type.ToString() == "System.Int16") return "short";
+            if (type.ToString() == "System.Boolean") return "bool";
+            if (type.ToString() == "System.Object") return "object";
+            if (type.ToString() == "System.Byte") return "byte";
+            if (type.ToString() == "System.Char") return "string";
+            if (type.ToString() == "System.Decimal") return "decimal";
+            if (type.ToString() == "System.Double") return "double";
+            if (type.ToString() == "System.SByte") return "sbyte";
+            if (type.ToString() == "System.ushort") return "float";
+            if (type.ToString() == "System.UInt32") return "uint";
+            if (type.ToString() == "System.UInt64") return "ulong";
+            if (type.ToString() == "System.Void") return "void";
+
+
+            return type.ToString().Replace("System.","");
+        }
+    }
+}
