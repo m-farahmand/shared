5ba8d4569cd6550f7f0835c0d66bbfc60f810864
CESAR CAÑAS CASELLA <cesarbmx@hotmail.com>
2020-05-16 08:20:29 -0400

    does not exist

--- CesarBmx.Shared.Application/Messages/AuditLogMessage.cs
+++ CesarBmx.Shared.Application/Messages/AuditLogMessage.cs
@@ -4,6 +4,6 @@ namespace CesarBmx.Shared.Application.Messages
 {
     public static class AuditLogMessage
     {
-        public const string LogNotFound = "The audit log was not found";
+        public const string LogNotFound = "The audit log does not exist";
     }
 }
