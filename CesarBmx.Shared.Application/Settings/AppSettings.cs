9f36f23bc87bbb188993fcd36d66e7ee8a70644e
CESAR CAÑAS CASELLA <cesarbmx@hotmail.com>
2020-10-27 08:19:26 -0700

    remove secret from appsettings

--- CesarBmx.Shared.Application/Settings/AppSettings.cs
+++ CesarBmx.Shared.Application/Settings/AppSettings.cs
@@ -6,6 +6,5 @@ namespace CesarBmx.Shared.Application.Settings
     public  class AppSettings
     {
         public string ApplicationId { get; set; }
-        public string Secret { get; set; }
     }
 }
